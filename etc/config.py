ed63aeb240ccfc263bf8eff5a58def30863885e2
Baburin Sergey <sbasba777@hotmail.com>
2016-06-07 14:04:03 +0300

    1.9.11

---
+++ etc/config.py
@@ -0,0 +1,9 @@
+﻿CONFIG = {                                                                      
+    # 'mode': 'wsgi',                                                           
+    'working_dir': '/home/box/web',                                           
+    # 'python': '/usr/bin/python',                                              
+    'args': (                                                                   
+        '--bind=0.0.0.0:8080',                      
+        'hello:app',                                                           
+    ),                                                                          
+}
---
+++ hello.py
@@ -0,0 +1,5 @@
+﻿def app(environ, start_response):
+	start_response("200 OK", [("Content-Type", "text/plain")])
+	queryString = environ["QUERY_STRING"]
+	responseBody = queryString.replace("&", "\n")
+	return responseBody
